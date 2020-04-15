2b3afbe7b1ad0ad70ca19ff452736f16cf4f926b
Guy Avraham <guy1812@gmail.com>
2020-04-15 14:42:30 +0300

    adding last purchase date + amount and adding curr balance

--- src/functions/existOnCrm.ts
+++ src/functions/existOnCrm.ts
@@ -1,26 +1,58 @@
 import * as _ from "lodash";
 import * as customers from "../apis/priority/customer";
 import * as accounts from "../apis/priority/accounts";
+import * as invoices from "../apis/priority/invoices";
 import "source-map-support/register";
-import {internalErrorResponse, noSuchCustomerResponse} from "../common/responses";
-import {adaptPhoneNum} from "../utils/phoneNumber";
+import {
+  internalErrorResponse,
+  noSuchCustomerResponse,
+} from "../common/responses";
+import { adaptPhoneNum } from "../utils/phoneNumber";
 
-const validResponse = params => ({
+const validResponse = (params) => ({
   statusCode: 200,
   body: JSON.stringify(
     {
-      ...params
+      ...params,
     },
     null,
     2
-  )
+  ),
 });
 
-async function getMoreData(id: String) {
-  const account = await accounts.findById(id);
-  console.log('*** account', account);
+async function getAccountBalance(id) {
+  return accounts.findById(id).then((accountsList) => {
+    const currAccount = _.first(accountsList);
+    return currAccount ? currAccount.BALANCE1 : null;
+  });
+}
 
-  return account;
+async function getLastPurchaseData(id) {
+  return invoices.findById(id).then((invoicesList) => {
+    const latestInvoice = _.first(invoicesList);
+    if (!latestInvoice)
+      return { lastPurchaseAmount: null, lastPurchaseDate: null };
+    return {
+      lastPurchaseAmount: latestInvoice.TOTPRICE,
+      lastPurchaseDate: latestInvoice.IVDATE,
+    };
+  });
+}
+
+async function getMoreData(id: String) {
+  const [accountBalance, latestInvoiceData] = await Promise.all([
+    getAccountBalance(id),
+    getLastPurchaseData(id),
+  ]);
+
+  const data = {
+    accountBalance,
+    ...latestInvoiceData,
+  };
+
+  console.log("*** data", data);
+
+  return data;
 }
 
 export async function validatePhoneNumberExists(data) {
@@ -39,27 +71,30 @@ export async function validatePhoneNumberExists(data) {
   }
 
   const persona = result[0];
-  console.log('*** persona', persona);
   const id = persona.CUSTNAME;
-  // const extraDate = await getMoreData(id);
+  const extraData = await getMoreData(id);
+  console.log("*** extraData", extraData);
 
   return validResponse({
     name: persona.CUSTDES,
     phoneNumber: persona.PHONE,
-    address: persona.ADDRESS2 ? `${persona.ADDRESS} ${persona.ADDRESS2}, ${persona.STATE}` : `${persona.ADDRESS}, ${persona.STATE}`,
+    address: persona.ADDRESS2
+      ? `${persona.ADDRESS} ${persona.ADDRESS2}, ${persona.STATE}`
+      : `${persona.ADDRESS}, ${persona.STATE}`,
     vat: persona.VATNUM,
     created: persona.CREATEDDATE,
     agent: persona.AGENTNAME,
     dob: persona.SPEC6,
-  })
+    ...extraData,
+  });
 }
 
 export async function existByPhone(event) {
   let body = JSON.parse(event.body);
   console.log("*** body", body);
-  if (!body.phoneNumber) return internalErrorResponse('No phoneNumber')
+  if (!body.phoneNumber) return internalErrorResponse("No phoneNumber");
 
-  return validatePhoneNumberExists(body).catch(error => {
+  return validatePhoneNumberExists(body).catch((error) => {
     console.log("error: ****", error);
     return internalErrorResponse(error.message);
   });
