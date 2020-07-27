d36f58fb806bc0711038a69dce3691adbc575897
Guy Avraham <guy1812@gmail.com>
2020-07-27 10:07:29 +0300

    top10 issue.

--- src/functions/utils/top10.ts
+++ src/functions/utils/top10.ts
@@ -1,29 +1,33 @@
 import * as _ from "lodash";
+import { Promise } from "bluebird";
 
 import * as invoicesApi from "../../apis/priority/invoices";
+
+const concurrency = 10;
 
 export async function reduceItemsFromInvoices(invoicesList) {
   const itemsCounter = {
     // "213123": { ... invoice, sum}
   };
 
-  await Promise.all(
-    invoicesList.map((invoice) =>
-      invoicesApi.findExtendedByInvoiceId(invoice).then((items) => {
-        _.map(items, (currItem) => {
-          if (itemsCounter[currItem.barcode]) {
-            // add to count
-            itemsCounter[currItem.barcode].sum += currItem.amount;
-          } else {
-            // new count
-            itemsCounter[currItem.barcode] = {
-              ..._.omit(currItem, ['amount']),
-              sum: currItem.amount,
-            };
-          }
-        });
-      })
-    )
+  await Promise.map(
+    invoicesList,
+    async (invoice) => {
+      const items = await invoicesApi.findExtendedByInvoiceId(invoice);
+      _.map(items, (currItem) => {
+        if (itemsCounter[currItem.barcode]) {
+          // add to count
+          itemsCounter[currItem.barcode].sum += currItem.amount;
+        } else {
+          // new count
+          itemsCounter[currItem.barcode] = {
+            ..._.omit(currItem, ["amount"]),
+            sum: currItem.amount,
+          };
+        }
+      });
+    },
+    { concurrency }
   );
 
   return itemsCounter;
